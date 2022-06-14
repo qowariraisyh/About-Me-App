db68dc33409004b07b883c3ee821ffc60d5f501e
reerah <qowariraaisyah@gmail.com>
2022-06-15 00:24:07 +0700

    Initial Commit

---
+++ About Me App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  About Me App
+//
+//  Created by Aisyah Qowarira on 6/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
